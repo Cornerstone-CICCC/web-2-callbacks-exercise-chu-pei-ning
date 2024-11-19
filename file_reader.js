3ce6853f38999c372c079ad2417f42ff27a95598
Julia_Chu <z3211232001@gmail.com>
2024-11-19 11:41:07 -0800

    finished

--- file_reader.js
+++ file_reader.js
@@ -1,5 +1,16 @@
 const fs = require("fs");
 
+//result `John Smith is 50 years old and his hoobies are swimming and dancing`
 fs.readFile("./firstname.txt", "utf-8", (err, data1) => {
-  // your code here
+  const firstname = data1
+  fs.readFile("./lastname.txt", "utf-8", (err, data2) => {
+    const lastname = data2
+    fs.readFile("./age.txt", "utf-8", (err, data3) => {
+      const age = data3
+      fs.readFile("./hobbies.txt", "utf-8", (err, arr) => {
+        const hobbies = JSON.parse(arr)
+        console.log(`${firstname} ${lastname} is ${age} years old and his hobbies are ${hobbies[0]} and ${hobbies[1]}.`);
+      })      
+    })
+  })
 })
